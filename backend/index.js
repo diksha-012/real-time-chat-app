2e9bd0052b9d572ea50ead0697fe20db746ef56f
Diksha <dikshamalik012@gmail.com>
2025-06-13 18:05:17 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("✅ Server listening on port 5000");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`✅ Server listening on port ${PORT}`);
 });
