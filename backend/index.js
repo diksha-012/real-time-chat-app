ea2341df2b8512456e48954b0f5d488cf2aa6d49
Diksha <dikshamalik012@gmail.com>
2025-06-13 19:06:00 +0530

    more change

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors()); // Enable CORS
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Your React frontend
+    origin: "https://real-time-chat-app-weld.vercel.app/", // Your React frontend
     methods: ["GET", "POST"]
   }
 });
